d90dbfb18dc22b1f572b22eaf81031e3e01a9c0b
Kaushal Damania <damaniakaushal4@gmail.com>
2021-05-26 09:52:53 +0530

    front end path

--- server/server.py
+++ server/server.py
@@ -1,10 +1,12 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 app = Flask(__name__)
 
-@app.route('/hello')
-def hello():
-    return "hi"
 
-if __name__ == "__main__":
+@app.route('/')
+def hello():
+    return render_template('app.html')
+
+
+if __name__ == '__main__':
     print("starting python flask server for Best Crop Prediction")
-    app.run()
+    app.run(debug=True, host='0.0.0.0')
